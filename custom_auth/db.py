01931c01012d049fe1316bf594fcaa94d8402e40
Antoine74861 <209369232+Antoine74861@users.noreply.github.com>
2025-05-01 11:08:46 +0200

    first commit

---
+++ custom_auth/db.py
@@ -0,0 +1,4 @@
+DB_PASSWORD = "test_db_pwd"
+DB_USER = "test_db_user"
+
+# add more
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="custom_auth",
+    packages=find_packages(),
+)
